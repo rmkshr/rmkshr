91a11f9478299e760a6d824db6f2ba876fd880e8
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-08-20 23:34:51 +0530

    50% completed push

--- src/main/java/com/spring/security/apisecurity/ApisecurityApplication.java
+++ src/main/java/com/spring/security/apisecurity/ApisecurityApplication.java
@@ -1,7 +1,16 @@
 package com.spring.security.apisecurity;
 
+import com.spring.security.apisecurity.domain.ApiUser;
+import com.spring.security.apisecurity.domain.Role;
+import com.spring.security.apisecurity.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ApisecurityApplication {
@@ -10,4 +19,44 @@ public class ApisecurityApplication {
 		SpringApplication.run(ApisecurityApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run (UserService userService) {
+		return args -> {
+
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_GUEST"));
+			userService.saveRole(new Role(null, "ROLE_PROXY"));
+
+			userService.saveUser(new ApiUser(null, "Ramkishore", "rmkshr", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "kishore", "kishore", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "vishnu", "vishnu", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "Linus", "linus", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "Suganya", "suganya", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "rootuser", "rootuser", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "denzel", "denzel", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "dave", "dave", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "jack", "jack", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "jill", "jill", "letmein", new ArrayList<Role>()));
+			userService.saveUser(new ApiUser(null, "hello", "hello", "letmein", new ArrayList<Role>()));
+
+			userService.addRoleToApiUser("rmkshr","ROLE_SUPER_ADMIN");
+			userService.addRoleToApiUser("rmkshr","ROLE_GUEST");
+			userService.addRoleToApiUser("rmkshr","ROLE_PROXY");
+			userService.addRoleToApiUser("rmkshr","ROLE_ADMIN");
+			userService.addRoleToApiUser("rmkshr","ROLE_USER");
+			userService.addRoleToApiUser("rmkshr","ROLE_ADMIN");
+			userService.addRoleToApiUser("rmkshr","ROLE_USER");
+			userService.addRoleToApiUser("rmkshr","ROLE_PROXY");
+			userService.addRoleToApiUser("rmkshr","ROLE_PROXY");
+			userService.addRoleToApiUser("rmkshr","ROLE_PROXY");
+			userService.addRoleToApiUser("rmkshr","ROLE_ADMIN");
+		};
+	}
 }
--- src/main/java/com/spring/security/apisecurity/service/UserServiceImpl.java
+++ src/main/java/com/spring/security/apisecurity/service/UserServiceImpl.java
@@ -6,13 +6,20 @@ import com.spring.security.apisecurity.repo.RoleRepo;
 import com.spring.security.apisecurity.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
 
@@ -30,15 +37,15 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void addRoleToApiUser(String userName, String roleName) {
-    log.info("Saving new role {} to user {}", roleName, userName);
-    ApiUser user = userRepo.findByUserName(userName);
-    Role role = roleRepo.findByName(roleName);
-    user.getRoles().add(role);
+        log.info("Saving new role {} to user {}", roleName, userName);
+        ApiUser user = userRepo.findByUserName(userName);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public ApiUser getUser(String userName) {
-    log.info("Fetching user {}", userName);
+        log.info("Fetching user {}", userName);
         return userRepo.findByUserName(userName);
     }
 
@@ -46,5 +53,21 @@ public class UserServiceImpl implements UserService{
     public List<ApiUser> getUsers() {
         log.info("Fetching all users");
         return userRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        ApiUser apiUser = userRepo.findByUserName(userName);
+        if(apiUser == null){
+            log.error("User not found in the Database");
+            throw new UsernameNotFoundException("User not found in the Database");
+        } else {
+            log.info("User found in the Database: {}", userName);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        apiUser.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(apiUser.getUserName(), apiUser.getPassword(),  authorities);
     }
 }
