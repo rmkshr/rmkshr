14ed998f846f3cff334fd98258ad54ea1f1f83c8
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-08-21 23:36:35 +0530

    Complete Spring security course [WORKING]

--- src/main/java/com/spring/security/apisecurity/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/spring/security/apisecurity/filter/CustomAuthenticationFilter.java
@@ -55,7 +55,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 //        response.setHeader("access_token", access_token);
