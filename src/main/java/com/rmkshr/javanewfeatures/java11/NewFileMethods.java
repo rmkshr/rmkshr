0352dbd73413913bc8a1a6b1f4ea6e32ae6860f5
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-10-09 23:16:15 +0530

    Java 12 Done

--- src/main/java/com/rmkshr/javanewfeatures/java11/NewFileMethods.java
+++ src/main/java/com/rmkshr/javanewfeatures/java11/NewFileMethods.java
@@ -11,7 +11,7 @@ public class NewFileMethods {
         //We can use the new readString and writeString static methods from the Files class:
 
         try {
-            Path path = Files.writeString(Files.createTempFile(Path.of("/Users/ramkishoremadheshwaran/Local"), "file_demo", ".txt"),
+            Path path = Files.writeString(Files.createTempFile(Path.of("/Users/ramkishoremadheshwaran/Local/javatempfiles"), "file_demo", ".txt"),
                     "First program with desktop setup");
             String fileContent = Files.readString(path);
             System.out.println(fileContent);
---
+++ src/main/java/com/rmkshr/javanewfeatures/java12/StringClassNewMethods.java
@@ -0,0 +1,22 @@
+package com.rmkshr.javanewfeatures.java12;
+
+public class StringClassNewMethods {
+
+    public static void main(String[] args) {
+
+        //If the parameter is greater than zero, new spaces will be inserted at the beginning of each line.
+        //On the other hand, if the parameter is less than zero, it removes spaces from the begging of each line.
+        //If a given line does not contain sufficient white space, then all leading white space characters are removed.
+        String text = "Hi guys this is Ramkishore";
+        text = text.indent(5);
+        System.out.println(text);
+        text = text.indent(-10);
+        System.out.println(text);
+
+        //The second new method is transform.
+        //It accepts a single argument function as a parameter that will be applied to the string.
+        String someString = "qwerty";
+        someString = someString.transform(value -> new StringBuilder(value).reverse().toString());
+        System.out.println(someString);
+    }
+}
