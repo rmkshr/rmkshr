1f292616c6e8b65df8910c1c5b23d5d55163a43b
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-10-09 19:04:47 +0530

    Java 11 Done. Java 12 Begins

---
+++ src/main/java/com/rmkshr/javanewfeatures/java11/LocalVariableSyntaxForLambda.java
@@ -0,0 +1,21 @@
+package com.rmkshr.javanewfeatures.java11;
+
+import lombok.NonNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class LocalVariableSyntaxForLambda {
+
+    public static void main(String[] args) {
+
+        //Support for using the local variable syntax (var keyword) in lambda parameters was added in Java 11.
+        //We can make use of this feature to apply modifiers to our local variables, like defining a type annotation:
+
+        List<String> nameList = Arrays.asList("Ram", "Sugan", "Vihaan");
+        String resultString = nameList.stream().map((@NonNull var x) -> x.toUpperCase(Locale.ROOT)).collect(Collectors.joining(","));
+        System.out.println(resultString);
+    }
+}
---
+++ src/main/java/com/rmkshr/javanewfeatures/java11/NotPredicateMethod.java
@@ -0,0 +1,21 @@
+package com.rmkshr.javanewfeatures.java11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class NotPredicateMethod {
+
+    public static void main(String[] args) {
+
+        //While not(isBlank) reads more naturally than isBlank.negate(),
+        // the big advantage is that we can also use not with method references, like not(String:isBlank).
+        List<String> sampleList = Arrays.asList("Java", "\n \n", "Kotlin", " ");
+        List withoutBlanks = sampleList.stream()
+                .filter(Predicate.not(String::isBlank))
+                .collect(Collectors.toList());
+
+        System.out.println(withoutBlanks);
+    }
+}
