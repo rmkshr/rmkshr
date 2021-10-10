f0aa2dda17c8db3e5b616ea196fe0f8f047d1622
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-10-11 00:26:27 +0530

    Change in login url

--- src/main/java/com/rmkshr/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/rmkshr/springsecurity/security/SecurityConfig.java
@@ -43,10 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/apiv3/login");
+        customAuthenticationFilter.setFilterProcessesUrl("/auth/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/apiv3/login/**", "/apiv3/refreshtoken/**").permitAll();
+        http.authorizeRequests().antMatchers("/auth/login/**", "/auth/refreshtoken/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/apiv3/**").hasAuthority(Constants.ROLE_ADMIN);
         http.authorizeRequests().antMatchers(POST, "/apiv3/**").hasAuthority(Constants.ROLE_ADMIN);
         http.authorizeRequests().antMatchers(GET, "/spring/v1/**").hasAuthority(Constants.ROLE_ADMIN);
