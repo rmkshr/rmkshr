f8828513a485f32a81944c761cb78d7b51357c66
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-10-10 19:07:08 +0530

    Java 12 Done

---
+++ src/main/java/com/rmkshr/javanewfeatures/java15/SealedClass.java
@@ -0,0 +1,16 @@
+package com.rmkshr.javanewfeatures.java15;
+
+import lombok.NonNull;
+
+//the goal of sealed classes is to allow individual classes to declare which types may be used as sub-types.
+// This also applies to interfaces and determining which types can implement them.
+//Sealed classes involve two new keywords — sealed and permits:
+
+public sealed interface SealedClass permits SampleImplementation {
+
+    @NonNull
+    String dataSet = "MORGAN_DATA_CENTER";
+
+}
+
+
---
+++ src/main/java/com/rmkshr/javanewfeatures/java16/StreamsToList.java
@@ -0,0 +1,16 @@
+package com.rmkshr.javanewfeatures.java16;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsToList {
+
+    public static void main(String[] args) {
+
+        //Java 16 introduces toList method in streams to reduce the boilerplate codes like Collectors.toList or Collectors.toSet
+        
+        List<Integer> numList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List filteredList = numList.stream().filter(n -> n == 2).toList();
+        System.out.println(filteredList);
+    }
+}
