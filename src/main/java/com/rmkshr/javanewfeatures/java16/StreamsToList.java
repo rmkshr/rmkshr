b63d6881529cb91825602f4b207757028a1ac2eb
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-10-10 19:08:45 +0530

    Java 12 Done

--- src/main/java/com/rmkshr/javanewfeatures/java16/StreamsToList.java
+++ src/main/java/com/rmkshr/javanewfeatures/java16/StreamsToList.java
@@ -8,7 +8,7 @@ public class StreamsToList {
     public static void main(String[] args) {
 
         //Java 16 introduces toList method in streams to reduce the boilerplate codes like Collectors.toList or Collectors.toSet
-        
+
         List<Integer> numList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         List filteredList = numList.stream().filter(n -> n == 2).toList();
         System.out.println(filteredList);
