e6c04d219083bc811e696ab290698ac530f81f1b
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-12-01 00:36:33 +0530

    EncryptionGCM and Jackson data bind changes

--- src/main/java/org/srv/jackson/JacksonObjectMapper.java
+++ src/main/java/org/srv/jackson/JacksonObjectMapper.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 
 public class JacksonObjectMapper {
 //Jackson is CASE SENSITIVE : case sensitivity between the json and the POJO class MATTERS!
+
+    /**
+     * Function used to marshal data between json and POJO class.
+     * @return
+     * @throws IOException
+     */
     public EmployeeJackson readJsonWithObjectMapper() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         EmployeeJackson employeeJackson = objectMapper.readValue(new File("/Users/ramkishoremadheshwaran/Documents/IntelliJ Workspace/rmkshr/src/main/java/org/srv/jackson/employee.json"), EmployeeJackson.class);
