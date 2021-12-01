40ae625f129d97e9327f7aae2f34c3dc5ca255cc
Ramkishore Madheshwaran <ramkishoremadheshwaran@gmail.com>
2021-12-01 21:06:08 +0530

    Redis Cache implementation : Working

---
+++ src/main/java/org/srv/redis/RedisTest.java
@@ -0,0 +1,13 @@
+package org.srv.redis;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RedisTest {
+
+    @Test
+    public void testRedisCache() {
+        String redisConnect = RedisUtils.redisConnect();
+        Assertions.assertEquals(redisConnect, "S.R.Vihaan");
+    }
+}
